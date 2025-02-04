8a48ab0a976eceea6e009c84ffb218da15da8df9
Gabriel-OliveiraP <gabriel.pereirao2005@gmail.com>
2025-02-04 06:31:59 -0300

    H2 database, test profile, JPA

--- src/main/java/com/gabrielprojeto/course/entities/User.java
+++ src/main/java/com/gabrielprojeto/course/entities/User.java
@@ -3,11 +3,23 @@ package com.gabrielprojeto.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id//para indicar que é a chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//Indicando que essa chave é autoincrementável
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
