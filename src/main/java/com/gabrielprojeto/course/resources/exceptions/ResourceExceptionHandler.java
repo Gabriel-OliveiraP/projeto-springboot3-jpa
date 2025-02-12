766beb90ce711d834b4bc9808cb37f5621cc37ec
Gabriel-OliveiraP <gabriel.pereirao2005@gmail.com>
2025-02-12 00:09:42 -0300

    Exception handling - findById

---
+++ src/main/java/com/gabrielprojeto/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.gabrielprojeto.course.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.gabrielprojeto.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	//exception name
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError fullError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(fullError);
+	}
+		
+}
